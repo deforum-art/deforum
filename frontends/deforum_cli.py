dcf14c84127cf06fe798bc099d4ccdf21ea78fd5
XmYx <miklos.mnagy@gmail.com>
2023-01-22 17:19:37 +0100

    added cli config

--- frontends/deforum_cli.py
+++ frontends/deforum_cli.py
@@ -1,10 +1,26 @@
 import argparse
+import yaml
+
+try:
+    with open("../configs/cli-modules.yaml") as f:
+        modules = yaml.safe_load(f)
+        f.close()
+except:
+    with open("configs/cli-modules.yaml") as f:
+        modules = yaml.safe_load(f)
+        f.close()
+
+for module in modules:
+    try:
+        __import__(module)
+    except Exception as e:
+        print(f"{module} could not be imported: {e}")
 
 class DreamCLI:
     def __init__(self):
         self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("--arg1", type=str, default="default value")
-        self.parser.add_argument("--arg2", type=int, default=0)
+        self.parser.add_argument("--width", type=int, default=512)
+        self.parser.add_argument("--height", type=int, default=512)
         self.args = self.parser.parse_args()
         print("Welcome to DeforumCLI")
         print("use 'dream' to infer with the engine")
@@ -14,6 +30,9 @@ class DreamCLI:
         print("Running dream method with args:", self.args)
     def help(self):
         print("Possible arguements:")
+        for key, value in self.args.__dict__.items():
+            print(key, value)
+
 
 if __name__ == "__main__":
     cli = DreamCLI()
