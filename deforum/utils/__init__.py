de686ca0091b0ad9345b5106f0f32bc69c6c44b0
aredden <alexander.h.redden@gmail.com>
2023-07-29 20:06:32 -0400

    Adding huggingface hub model type fetcher

--- deforum/utils/__init__.py
+++ deforum/utils/__init__.py
@@ -1,3 +1,4 @@
 from .image_utils import ImageHandler, ImageReadMode
 from .pytorch_optimizations import channels_last, enable_optimizations
 from .string_parsing import normalize_text
+from .huggingface_utils import find_model_type
